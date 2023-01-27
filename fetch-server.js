0a6b725cdeb7402acbe725a900777cdd01f96095
rasaqakewusure <74925863+rasaqakewusure@users.noreply.github.com>
2023-01-27 13:46:52 +0000

    commit

--- fetch-server.js
+++ fetch-server.js
@@ -150,8 +150,13 @@ app.use(function(req, res) {
 });
 
 /// listening on port 3000
-app.listen(3000, function() {
-    console.log("App started on port 3000");
+//app.listen(3000, function() {
+    //console.log("App started on port 3000");
+//});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function(){
+    console.log("App started on port: " + port);
 });
 
 
