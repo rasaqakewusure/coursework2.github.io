f6a10c671b7ead41441745092070685955b0e55a
rasaqakewusure <74925863+rasaqakewusure@users.noreply.github.com>
2023-02-05 08:28:58 +0000

    aws deployment

--- static-file-fun/app.js
+++ static-file-fun/app.js
@@ -20,6 +20,8 @@ app.use(function(req, res) {
 res.status(404);
 res.send("File not found!");
 });
-app.listen(3000, function() {
-console.log("App started on port 3000");
+
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+console.log("App started on port: " + port);
 });
