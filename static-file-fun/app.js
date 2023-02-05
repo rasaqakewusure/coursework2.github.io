7f68c22b7d4b4862057df0ef8830a6627754c8ac
rasaqakewusure <74925863+rasaqakewusure@users.noreply.github.com>
2023-02-05 08:06:25 +0000

    Add files via upload

---
+++ static-file-fun/app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var morgan = require("morgan"); 
+var path = require("path");
+var fs = require("fs");
+var app = express();
+
+app.use(morgan("short"));
+
+app.use(function(req, res, next) {
+    console.log("Request IP: " + req.url);
+    console.log("Request date: " + new Date());
+    next();
+    });
+
+var staticPath = path.join(__dirname, "static");
+
+app.use(express.static(staticPath));
+
+app.use(function(req, res) {
+res.status(404);
+res.send("File not found!");
+});
+app.listen(3000, function() {
+console.log("App started on port 3000");
+});
